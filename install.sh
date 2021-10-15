85767514de8abf8e63eb2fb52f748747b808c6e1
Rhincodon_ri2h <acceptedzhs@qq.com>
2021-10-15 08:39:15 +0800

    make it easier to install vim-plug; modify install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ function get_time(){
 
 function print_info(){
     local choice
-    echo "This is an alpha version and many features are currently unavailable. (like custom plugins)"
+    echo "Please read README.md first and install some dependencies manually."
     echo -n "Continue? [y/N] "
     read choice
     if [ "$choice" != "y" ] && [ "$choice" != "Y" ];then
@@ -18,7 +18,7 @@ function print_info(){
 function backup_file(){
     local cur=`get_time`
     local choice
-    echo "It's going to backup your own vim configuration."
+    echo "Backup old vim configuration..."
     if [ -e ~/.vimrc ];then
         echo -n "~/.vimrc found. Backup? [Y/n] "
         read choice
@@ -37,25 +37,40 @@ function backup_file(){
 }
 
 function install_font(){
+    echo Installing FiraCode Nerd Font...
     mkdir -p ~/.local/share/fonts/firacode
     cp $(pwd)/fonts/*.otf ~/.local/share/fonts/firacode
+    echo Please change your terminal font to FiraCode Nerd Font.
+    sleep 0.5
 }
 
-function install_myvimrc(){
+function remove_old_configuration(){
     echo Removing old configuration...
     rm -i ~/.vimrc
     rm -rf ~/.vim
+    sleep 0.5
+}
+
+function copy_files(){
     echo Copying files...
     ln -s $(pwd)/.vimrc ~/.vimrc
     mkdir -p ~/.vim/autoload
     sleep 0.5
+}
 
+function finally_print_info(){
     echo
     echo Done!
     echo Open vim to complete the configuration.
 }
 
-print_info
-backup_file
-install_font
+function install_myvimrc(){
+    print_info
+    backup_file
+    install_font
+    remove_old_configuration
+    copy_files
+    finally_print_info
+}
+
 install_myvimrc
